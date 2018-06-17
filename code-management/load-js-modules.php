aa1345e2a9eda087b222faceefbcca2fb512d09f
tjespe <tordjohanespe@gmail.com>
2018-06-17 15:55:02 +0200

    Attempted to minify JS module CSS

--- code-management/load-js-modules.php
+++ code-management/load-js-modules.php
@@ -8,7 +8,13 @@ function loadModules(...$modules) {
   foreach ($modules as $module) {
     $content = file_get_contents("$libURL/src/$module.js");
     $code .= $content;
-    if (substr($content, 0, 6) === "//#CSS") $css .= str_replace("`", "\`", file_get_contents("$libURL/css/$module.css"));
+    if (substr($content, 0, 6) === "//#CSS") {
+      $tmphandle = tmpfile();
+      $tmpfname = stream_get_meta_data($tmphandle)["uri"];
+      fwrite($tmphandle, file_get_contents("$libURL/css/$module.css"));
+      $css .= str_replace("`", "\`", shell_exec("java -jar ".__DIR__."/../third-party/closure-stylesheets.jar $tmpfname"));
+      fclose($tmphandle);
+    }
   }
   $code .= "document.querySelector('style').innerText += `$css`;";
   return $code;
