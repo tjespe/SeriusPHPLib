d15d2d01ed1baba69e42cdbca49d653db84129f8
tjespe <tordjohanespe@gmail.com>
2018-07-26 19:06:00 +0200

    Made offline compilation possible

--- code-management/load-js-modules.php
+++ code-management/load-js-modules.php
@@ -1,15 +1,21 @@
 <?php
 
 include_once __DIR__."/../runtime/set-settings.php";
+include_once __DIR__."/../runtime/get-code.php";
 include_once __DIR__."/minify-code.php";
 
 function load_js_modules($modules) {
   if (!count($modules)) return "";
-  $libURL = DEVMODE ? __DIR__."/../../../../../SeriusJSLib" : "https://raw.githubusercontent.com/tjespe/SeriusJSLib/master";
+  $libURLs = [__DIR__."/../../../../../SeriusJSLib", "https://raw.githubusercontent.com/tjespe/SeriusJSLib/master"];
+  [$libURL, $altLibURL] = DEVMODE ? $libURLs : array_reverse($libURLs);
   $css = "";
   $code = "window.dependencies = ".json_encode($modules).";\n";
   foreach ($modules as $module) {
     $content = file_get_contents("$libURL/src/$module.js");
+    if (!$content) {
+      $content = file_get_contents("$altLibURL/src/$module.js");
+      if ($content) [$libURL, $altLibURL] = [$altLibURL, $libURL];
+    }
     $code .= $content;
     if (substr($content, 0, 6) === "//#CSS") $css .= str_replace("`", "\`", file_get_contents("$libURL/css/$module.css"));
   }
